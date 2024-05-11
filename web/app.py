bd4b479b152499af0dbfc0aa162f043fdc7af7ca
zhangliting <1196113942@qq.com>
2024-05-11 20:07:08 +0800

    admainUI design (including authentication)

--- web/app.py
+++ web/app.py
@@ -65,5 +65,92 @@ def smartCenter():
     return render_template('smartCenter.html')
 
 
+@app.route('/admain')
+def admain():
+    return render_template('admain/login.html')
+
+
+
+
+@app.route('/do_admain_login', methods=['POST'])
+def do_admain_login():
+    # 这里可以添加验证逻辑，验证管理员身份（用户名和密码）
+    # 如果验证成功，重定向到新页面
+    return redirect(url_for('admain_main'))
+
+
+@app.route('/admain_main')
+def admain_main():
+    return render_template('admain/main.html')
+
+
+@app.route('/admain_left')
+def admain_left():
+    return render_template('admain/left.html')
+
+@app.route('/admain_top')
+def admain_top():
+    return render_template('admain/top.html')
+
+@app.route('/admain_index')
+def admain_right():
+    return render_template('admain/index.html')
+
+@app.route('/admain_default')
+def admain_default():
+    return render_template('admain/default.html')
+
+@app.route('/admain_operateList')
+def admain_operateList():
+    return render_template('admain/operateList.html')
+    
+@app.route('/admain_searchList')
+def admain_searchList():
+    return render_template('admain/searchList.html')
+
+@app.route('/admain_form')
+def admain_form():
+    return render_template('admain/form.html')
+
+@app.route('/admain_tab')
+def admain_tab():
+    return render_template('admain/tab.html')
+
+@app.route('/admain_imgtable')
+def admain_imgtable():
+    return render_template('admain/imgtable.html')
+
+@app.route('/admain_imglist')
+def admain_imglist():
+    return render_template('admain/imglist.html')
+
+@app.route('/admain_imglist1')
+def admain_imglist1():
+    return render_template('admain/imglist1.html')
+
+@app.route('/admain_tools')
+def admain_tools():
+    return render_template('admain/tools.html')
+
+@app.route('/admain_filelist')
+def admain_filelist():
+    return render_template('admain/filelist.html')
+
+@app.route('/admain_computer')
+def admain_computer():
+    return render_template('admain/computer.html')
+
+@app.route('/admain_error')
+def admain_error():
+    return render_template('admain/error.html')
+
+
+
+@app.route('/logout_success')
+def logout_success():
+    return render_template('mainInfo.html')
+
+
 if __name__ == '__main__':
     app.run(host='127.0.0.1', debug=True)
+
